785620c75a7588e628283cf84011f4e901cba4ea
marduklev <36476590+marduklev@users.noreply.github.com>
2021-10-06 09:11:14 +0200

    some things

---
+++ scripts/playlistplayoffset.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import xbmc
+import xbmcgui
+
+playlist = xbmc.PlayList(0)
+current = playlist.getposition()
+selected = xbmc.getInfoLabel('ListItem.CurrentItem')
+index = int(selected) - int(current) -1
+xbmc.executebuiltin('Playlist.PlayOffset(%s)' % (index))
+
