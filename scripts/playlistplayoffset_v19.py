31aad3f9147cccd92e91be5c08323d2b684e549e
marduklev <36476590+marduklev@users.noreply.github.com>
2021-12-07 15:56:55 +0100

    update
    - rework skin.script for playoffset playlist (fix leia,working now for booth skin versions)

--- scripts/playlistplayoffset_v19.py
+++ scripts/playlistplayoffset_v19.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 import xbmc
 
-container_id = xbmc.getInfoLabel('System.CurrentControlID')
 playlistid = 1 if xbmc.getCondVisibility('Player.HasVideo') else 0
 playlist = xbmc.PlayList(playlistid)
 current = playlist.getposition()
-selected = xbmc.getInfoLabel('Container(%s).Position' % (container_id))
-index = int(selected) - int(current)
+selected = xbmc.getInfoLabel('ListItem.CurrentItem')
+index = int(selected) - int(current) -1
 xbmc.executebuiltin('Playlist.PlayOffset(%s)' % (index))
+
